7dbd44b41550e58b4b91dd3275dbc5421e5c619b
Luka Korica <lukakorica@gmail.com>
2022-08-05 23:22:36 +0200

    Initial Commit

---
+++ prodza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prodza
+//
+//  Created by Luka Korica on 8/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
